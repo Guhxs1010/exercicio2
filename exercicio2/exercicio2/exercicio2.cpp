12943babc5482a95969571224a71c7a790d66357
Guhxs1010 <133364786+Guhxs1010@users.noreply.github.com>
2024-04-12 16:54:43 -0300

    Add files via upload

---
+++ exercicio2/exercicio2/exercicio2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+int main(int arc, char* argv[]) {
+	/*int sorvetes = 27;
+	int valor1 = 150;
+	int valor2 = 125;
+
+	int mult1 = sorvetes * valor1;
+	int mult2 = sorvetes * valor2;
+
+	if (sorvetes <= 25) {
+		cout << mult1;
+	}
+	else if (sorvetes >= 26) {
+		cout << mult2;
+	}
+	return 0;*/
+
+	/*int numero = 11;
+	int calculo = numero + 10 * 2;
+	int calculo2 = numero + 9 * 3 -8;
+	if (numero > 10) {
+		cout << calculo;
+	}
+	else if(numero <10) {
+		cout << calculo2;
+	}
+	return 0;
+	*/
+
+	/*int numero = +10;
+	if (numero > 0) {
+		cout << "positivo";
+	}
+	else if (numero < 0) {
+		cout << "negativo";
+		}
+	 return 0;*/
+	
+	/*int numero1 = 30;
+	int numero2 = 20;
+	if (numero1 > numero2) {
+		cout << numero1;
+	}
+	else if (numero2 > numero1) {
+		cout << numero2;
+	}
+	return 0;*/
+
+	/*int nota1 = 8;
+	int nota2 = 10;
+	int calculo = nota1 + nota2 / 2;
+	if (calculo >= 7) {
+		cout << "Aprovado";
+	}
+	else if (calculo < 7) {
+		cout << "Reprovado";
+	}
+	return 0;*/
+
+}
